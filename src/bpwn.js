9ffa0585bafd8cd98ef4fa4878715c710681b8e3
zTags <tagsunderscore@gmail.com>
2021-10-09 18:37:53 +0200

    incredibly patchy code to test for a text/spwn object

--- src/bpwn.js
+++ src/bpwn.js
@@ -1,1 +1,28 @@
-// pretend theres really cool code in here
+function getObjEncoded(objinfo) {
+    var objEncoded = "";
+    return objEncoded;
+}
+
+function createLevel(spwnCode) {
+
+}
+
+function bpwn() {
+    console.log("loading BPWN...");
+    var scripts = document.scripts;
+    var isBPWNCodeFound = false;
+    var BPWNElement = undefined;
+    for (var i = 0; i < scripts.length; i++) {
+        if(scripts[i].type == "text/spwn") {
+            isBPWNCodeFound = true;
+            BPWNElement = scripts[i];
+        }
+    }
+
+    if(!isBPWNCodeFound) {
+        alert("BPWN code not found, see console for more information.");
+        console.log("BPWN code not found; did you set the `type` attr of a <script> element to \"text/spwn\"?")
+    } else {
+        // continue with bpwn-ing
+    }
+}
